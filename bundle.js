eb3dc2c666001c63945657cd045e5c532f01bff0
mbr84 <matthew.bramfeld@gmail.com>
2016-10-07 17:04:36 -0700

    whyyyy

--- bundle.js
+++ bundle.js
@@ -54,40 +54,44 @@
 /***/ function(module, exports) {
 
 	const formatTime = (time) => {
-	  return time > 9 || time !== 0 ? time : '0' + time;
+	  return (time < 10 ? "0" + time : time)
 	};
 	
 	const timerHTML = (time, unit) => {
 	  const html = document.createElement('div');
 	  html.innerHTML = `
-	  <div class='unit-wrapper'>
-	    <div class='time'>${time}</div>
-	    <div class='unit'>${unit}</div>
+	  <div class='${unit}'>
+	    <div class='time'>${formatTime(time)}</div>
+	    <div class='unit'>${unit.toUpperCase()}</div>
 	  </div>
 	  `;
-	
-	  return html
+	  if (unit === "seconds") { html.class = "unit-wrapper seconds"; }
+	  return html;
 	};
 	
 	const countdown = () => {
 	  const interval = setInterval(function () {
 	    const timerWrapper = document.createElement('div');
 	    const timer = document.getElementById('timer');
-	    const colon = document.createElement('div')
 	
 	    const time = Date.parse('2017-1-1') - Date.parse(new Date);
-	    const hours = formatTime(Math.floor(time / (1000 * 60 * 60)));
-	    timerWrapper.appendChild(timerHTML(formatTime(hours), 'hours'));
-	    timerWrapper.innerHTML += "<div class='colon'> : </div>";
+	    if (time > 0) {
+	      const hours = formatTime(Math.floor(time / (1000 * 60 * 60)));
+	      timerWrapper.appendChild(timerHTML(hours, 'hours'));
+	      timerWrapper.innerHTML += "<div class='colon'> : </div>";
 	
-	    const minutes = formatTime(Math.floor((time / 1000 / 60) % 60));
-	    timerWrapper.appendChild(timerHTML(formatTime(minutes), 'minutes'));
-	    timerWrapper.innerHTML += "<div class='colon'> : </div>";
+	      const minutes = formatTime(Math.floor((time / 1000 / 60) % 60));
+	      timerWrapper.appendChild(timerHTML(minutes, 'minutes'));
+	      timerWrapper.innerHTML += "<div class='colon'> : </div>";
 	
-	    const seconds = formatTime(Math.floor((time / 1000) % 60));
-	    timerWrapper.appendChild(timerHTML(formatTime(seconds), 'seconds'));
+	      const seconds = formatTime(Math.floor((time / 1000) % 60));
+	      seconds.className = "seconds"
+	      timerWrapper.appendChild(timerHTML(seconds, 'seconds'));
 	
-	    timer.innerHTML = timerWrapper.innerHTML
+	      timer.innerHTML = timerWrapper.innerHTML
+	    } else {
+	      timer.text = "HAPPY NEW YEAR"
+	    }
 	  }, 1000);
 	};
 	
